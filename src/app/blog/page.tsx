4cff4fd70fede127271c342275985b06a03e2b19
RiccardoAD <rick_95@libero.it>
2025-03-12 16:27:20 +0100

    contact

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,8 @@
 // app/blog/page.js
+//route annidate
 
 export default function  Blog (){
-    return <h1> Blog design page 
+    return <h1> Blog design page </h1>
 
-    </h1>
+
 }
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Contact(){
+    return 
+    <>
+    <h1> Contact page
+        
+    </h1>
+    <h3> for About </h3> ; <Link href="/about">about </Link>    </>
+
+}
